c177ec6755a04a283c01ea289af0a42189b7f31e
GatoArtStudio <hackhcctecno@gmail.com>
2025-11-08 12:43:57 -0500

    Initial commit

---
+++ src/main/java/art/gatoartstudio/testingPackage/TestingPackage.java
@@ -0,0 +1,17 @@
+package art.gatoartstudio.testingPackage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TestingPackage extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
